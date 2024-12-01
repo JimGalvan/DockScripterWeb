245a4eaf9658fbc67784dcb03a65b849f71f793d
Jim Galvan <58128571+JimGalvan@users.noreply.github.com>
2024-11-30 23:36:19 -0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactxx
+          Learn Reacts
         </a>
       </header>
     </div>
